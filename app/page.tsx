9aa4463d92c86ffbd0776bbf7ff00a56309ab489
mesutkacmaz <mesut.kacmaz@outlook.com>
2024-12-01 21:30:22 +0300

    Change metadata & replace favicon

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
       <p className='mb-12 mt-7 text-2xl lg:text-3xl opacity-75'>
         Browse more than{' '}
-        <span className='text-[#a4f839] font-bold italic underline'>
+        <span className='text-accent font-bold italic underline'>
           10,000 events
         </span>{' '}
         around you
@@ -18,7 +18,7 @@ export default function Home() {
       <form className='w-full sm:w-[580px]'>
         <input
           type='text'
-          className='w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10'
+          className='w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10'
           placeholder='Search events in any city...'
           spellCheck={false}
         />
