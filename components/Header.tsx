1768b2156438c731f7ff9b7672cb015e5860b738
mesutkacmaz <mesut.kacmaz@outlook.com>
2024-12-01 21:48:33 +0300

    Add active link effect with framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import Link from 'next/link'
 import Logo from './Logo'
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
+import { motion } from 'motion/react'
 
 const routes = [
   { name: 'Home', path: '/' },
@@ -7,17 +12,32 @@ const routes = [
 ]
 
 export default function Header() {
+  const pathname = usePathname()
+
   return (
     <header className='flex justify-between items-center border-b border-white/10 h-14 sm:px-9 px-3'>
       <Logo />
-      <nav>
-        <ul className='flex gap-x-6 text-sm'>
+      <nav className='h-full'>
+        <ul className='flex gap-x-6 h-full text-sm'>
           {routes.map((route) => (
             <li
               key={route.path}
-              className='text-white/50 hover:text-white transition'
+              className={clsx(
+                'relative flex items-center hover:text-white transition',
+                {
+                  'text-white': pathname === route.path,
+                  'text-white/50': pathname !== route.path,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+
+              {pathname === route.path && (
+                <motion.div
+                  layoutId='header-active-link'
+                  className='bg-accent h-1 w-full absolute bottom-0'
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
