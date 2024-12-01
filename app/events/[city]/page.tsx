3e04a6b0b0779d53f2852aad2f4027c3ece46e2b
mesutkacmaz <mesut.kacmaz@outlook.com>
2024-12-01 23:20:14 +0300

    Fetch events from external api & add types for fetched events

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -1,9 +1,32 @@
 import Title from '@/components/Title'
+import { EventoEvent } from '@/lib/types'
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: Promise<{
+    city: string
+  }>
+}
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const { city } = await params
+
+  const res = await fetch(
+    'https://bytegrad.com/course-assets/projects/evento/api/events?city=austin'
+  )
+
+  const events: EventoEvent[] = await res.json()
+
   return (
     <main className='flex flex-col items-center py-24 px-[20px] min-h-[110dvh]'>
-      <Title>Events is Austin</Title>
+      <Title>
+        {city === 'all' && 'All Events'}
+        {city !== 'all' &&
+          `Events is ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </Title>
+
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   )
 }
