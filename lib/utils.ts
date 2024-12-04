bf4aadb7ff901c04d07e59f479c1d789228229c8
mesutkacmaz <mesut.kacmaz@outlook.com>
2024-12-04 22:19:27 +0300

    Use cache for prisma calls

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@ import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import prisma from "./db";
 import { notFound } from "next/navigation";
+import { unstable_cache } from "next/cache";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,7 +12,7 @@ export function capitalize(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
-export async function getEvents(city: string, page = 1) {
+export const getEvents = unstable_cache(async (city: string, page = 1) => {
   const events = await prisma.eventoEvent.findMany({
     where: { city: city === "all" ? undefined : { equals: capitalize(city) } },
     orderBy: { date: "asc" },
@@ -30,9 +31,9 @@ export async function getEvents(city: string, page = 1) {
   }
 
   return { events, totalCount };
-}
+});
 
-export async function getEvent(slug: string) {
+export const getEvent = unstable_cache(async (slug: string) => {
   const event = await prisma.eventoEvent.findUnique({ where: { slug } });
 
   if (!event) {
@@ -40,4 +41,4 @@ export async function getEvent(slug: string) {
   }
 
   return event;
-}
+});
